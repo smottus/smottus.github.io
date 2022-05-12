85de4b8301081ed92c08535ddfb637f97d51da22
sinilsso <sinilsso@kth.se>
2022-05-12 17:58:22 +0200

    Added growth when eating apples and collision so the snake cant go through itself

--- snake.js
+++ snake.js
@@ -1,99 +1,132 @@
+// Canvas dimensions:
 // 21x19
 // 672x608
 // 17x17
 // 544x544
 
-var cvs = document.getElementById("myCanvas");
-var ctx = cvs.getContext("2d");
-ctx.font = "50px Arial";
-ctx.fillStyle = "white";
+const cvs = document.getElementById("myCanvas");
+const ctx = cvs.getContext("2d");
+ctx.font = "40px Arial";
+ctx.fillStyle = "#efeff1";
 
-var background = new Image();
+const background = new Image();
 background.src = "pics/bg.png";
 
-var apple = new Image();
+const apple = new Image();
 apple.src = "pics/apple.png";
 
-var orm = new Image();
-orm.src = "pics/snake.png";
+const snakeImg = new Image();
+snakeImg.src = "pics/snake.png";
 
-var time = 100;
-var ruta = 32;
-var points = 0;
+const rows = (columns = 17);
+const time = 1000;
+const square = 32;
+let points = 0;
+let snake = [];
+let speed = 10; // Implement an increase when you get more points.
 
-var snake = [];
 snake[0] = {
-  x : 9*ruta,
-  y : 11*ruta
+  x: 9 * square,
+  y: 11 * square,
 };
-var dir = "RIGHT";
-var appleX = ruta + Math.floor(Math.random() * 17)*ruta;
-var appleY = 3*ruta + Math.floor(Math.random() * 17)*ruta;
+
+let dir = "right";
+let appleX = square + Math.floor(Math.random() * rows) * square;
+let appleY = 3 * square + Math.floor(Math.random() * columns) * square;
 
 window.addEventListener("keydown", (e) => {
-    switch (e.key) {
-        case "ArrowLeft":
-        case "A":
-        case "a":
-            if (dir !== "RIGHT") {
-                dir = "LEFT";
-            }  break;
-        case "ArrowRight":
-        case "D":
-        case "d":
-            if (dir !== "LEFT") {
-                dir = "RIGHT";
-            }  break;
-        case "ArrowUp":
-        case "W":
-        case "w":
-            if (dir !== "DOWN") {
-                dir = "UP";
-            }  break;
-        case "ArrowDown":
-        case "S":
-        case "s":
-            if (dir !== "UP") {
-                dir = "DOWN";
-            }  break;
-        case "R":
-        case "r":
-        case "Enter":
-            location.reload();
-}});
-
-function draw(){
-  ctx.drawImage(background,0,0);
-  ctx.fillText(points, 2*ruta, 1.6*ruta);
-  ctx.drawImage(apple,1*ruta, .6 *ruta)
-  ctx.drawImage(apple,appleX,appleY);
-  ctx.drawImage(orm,snake[0].x,snake[0].y);
-  switch(dir) {
-    case "LEFT":
-      snake[0].x -= ruta;
+  switch (e.key) {
+    case "ArrowLeft":
+    case "A":
+    case "a":
+      if (dir !== "right") {
+        dir = "left";
+      }
       break;
-    case "UP":
-      snake[0].y -= ruta;
+    case "ArrowRight":
+    case "D":
+    case "d":
+      if (dir !== "left") {
+        dir = "right";
+      }
       break;
-    case "RIGHT":
-      snake[0].x += ruta;
+    case "ArrowUp":
+    case "W":
+    case "w":
+      if (dir !== "down") {
+        dir = "up";
+      }
       break;
-    case "DOWN":
-      snake[0].y += ruta;
+    case "ArrowDown":
+    case "S":
+    case "s":
+      if (dir !== "up") {
+        dir = "down";
+      }
       break;
-  };
-
-  if(snake[0].x === appleX && snake[0].y === appleY){
-    appleX = ruta + Math.floor(Math.random() * 17)*ruta;
-    appleY = 3*ruta + Math.floor(Math.random() * 17)*ruta;
-    points++;
+    case "R":
+    case "r":
+    case "Enter":
+      location.reload();
   }
+});
 
-  if(snake[0].x===0||snake[0].x===18*ruta||snake[0].y===2*ruta||snake[0].y===20*ruta){
-    clearInterval(intervalID);
+function ouroboros() {
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+      return true;
+    }
   }
-
+  return false;
 }
 
+function draw() {
+  ctx.drawImage(background, 0, 0);
+  ctx.fillText(points.toString(), 2.1 * square, 1.6 * square);
+  ctx.drawImage(apple, square, 0.6 * square);
+  ctx.drawImage(apple, appleX, appleY);
 
-var intervalID = setInterval(draw,time);
+  for (let i = 0; i < snake.length; i++) {
+    ctx.drawImage(snakeImg, snake[i].x, snake[i].y);
+  }
+
+  switch (dir) {
+    case "left":
+      snake[0].x -= square;
+      break;
+    case "up":
+      snake[0].y -= square;
+      break;
+    case "right":
+      snake[0].x += square;
+      break;
+    case "down":
+      snake[0].y += square;
+      break;
+  }
+
+  if (snake[0].x === appleX && snake[0].y === appleY) {
+    appleX = square + Math.floor(Math.random() * rows) * square;
+    appleY = 3 * square + Math.floor(Math.random() * columns) * square;
+    points++;
+  } else if (snake.length > 1) {
+    snake.pop();
+  }
+
+  let nextHead = {
+    x: snake[0].x,
+    y: snake[0].y,
+  };
+
+  if (
+    snake[0].x === 0 ||
+    snake[0].x === 18 * square ||
+    snake[0].y === 2 * square ||
+    snake[0].y === 20 * square ||
+    ouroboros()
+  ) {
+    clearInterval(intervalID);
+  }
+  snake.unshift(nextHead);
+}
+let intervalID = setInterval(draw, time / speed);
