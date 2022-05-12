b240ecafb35a654310e559178e30f05f2d6de0c5
sinilsso <sinilsso@kth.se>
2022-05-12 13:50:11 +0200

    made a change

--- snake.js
+++ snake.js
@@ -20,6 +20,7 @@ orm.src = "pics/snake.png";
 var time = 100;
 var ruta = 32;
 var points = 0;
+
 var snake = [];
 snake[0] = {
   x : 9*ruta,
@@ -34,23 +35,27 @@ window.addEventListener("keydown", (e) => {
         case "ArrowLeft":
         case "A":
         case "a":
-            dir = "LEFT";
-            break;
+            if (dir !== "RIGHT") {
+                dir = "LEFT";
+            }  break;
         case "ArrowRight":
         case "D":
         case "d":
-            dir = "RIGHT";
-            break;
+            if (dir !== "LEFT") {
+                dir = "RIGHT";
+            }  break;
         case "ArrowUp":
         case "W":
         case "w":
-            dir = "UP";
-            break;
+            if (dir !== "DOWN") {
+                dir = "UP";
+            }  break;
         case "ArrowDown":
         case "S":
         case "s":
-            dir = "DOWN";
-            break;
+            if (dir !== "UP") {
+                dir = "DOWN";
+            }  break;
         case "R":
         case "r":
         case "Enter":
@@ -59,9 +64,9 @@ window.addEventListener("keydown", (e) => {
 
 function draw(){
   ctx.drawImage(background,0,0);
-  ctx.fillText(points, 2*ruta, 2*ruta);
+  ctx.fillText(points, 2*ruta, 1.6*ruta);
+  ctx.drawImage(apple,1*ruta, .6 *ruta)
   ctx.drawImage(apple,appleX,appleY);
-
   ctx.drawImage(orm,snake[0].x,snake[0].y);
   switch(dir) {
     case "LEFT":
@@ -78,13 +83,13 @@ function draw(){
       break;
   };
 
-  if(snake[0].x == appleX && snake[0].y == appleY){
+  if(snake[0].x === appleX && snake[0].y === appleY){
     appleX = ruta + Math.floor(Math.random() * 17)*ruta;
     appleY = 3*ruta + Math.floor(Math.random() * 17)*ruta;
     points++;
   }
 
-  if(snake[0].x==0||snake[0].x==18*ruta||snake[0].y==2*ruta||snake[0].y==20*ruta){
+  if(snake[0].x===0||snake[0].x===18*ruta||snake[0].y===2*ruta||snake[0].y===20*ruta){
     clearInterval(intervalID);
   }
 
